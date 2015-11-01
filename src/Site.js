b42d7ad8f9d436365fbe40be992f892b91928ca1
yangdy <anvilhorn@163.com>
2015-11-01 10:29:51 +0800

    Components

---
+++ src/Site.js
@@ -0,0 +1,47 @@
+var React = require('react');
+
+const styles = {
+    active: {
+        display: 'inherit'
+    },
+    inactive: {
+        display: 'none'
+    }
+};
+
+class Site extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            active: false
+        };
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle() {
+        this.setState({
+            active: !this.state.active
+        });
+    }
+
+    render () {
+        const stateStyle = this.state.active ? styles.active : styles.inactive;
+        return (
+            <section>
+                <a onClick={this.toggle}>{this.props.summary}</a>
+                <p style={stateStyle}>
+                    {this.props.details}
+                </p>
+            </section>
+        );
+    }
+
+}
+
+Site.propTypes = {
+    summary: React.PropTypes.string.isRequired,
+    details: React.PropTypes.string.isRequired
+};
+
+export default Site;
